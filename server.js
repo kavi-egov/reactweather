ddd9939b934152900f7171fbb489c9d06869e78f
kaviyarasan1993 <kaviyarasan.p@riflexions.com>
2018-04-25 23:05:35 +0530

    sevrer env value added

--- server.js
+++ server.js
@@ -5,12 +5,14 @@ var expressServer = require('express');
 // load the app by calling the express library
 var app = expressServer();
 
+const port = process.env.PORT || 3000;
+
 // point to the location from where the resources should be loaded in to the server (use method)
 //express static method to load the folder in which entry point is avavilable
 app.use(expressServer.static('public'));
 
 //listen method to start the server
 //port no and function to be invoked a sarguments as arguments
-app.listen(3000,function(){
-  console.log(' app is running @ port 3000');
+app.listen(port,function(){
+  console.log(' app is running @ port '+ port);
 });
